6b8e23f6d01fc099ff2527987d796f67b021f9db
Tim Trice <tim.trice@gmail.com>
2018-08-10 14:46:18 +0000

    Add script to install uninstalled pkgs

---
+++ code/01_packages.R
@@ -0,0 +1,15 @@
+list.of.packages <-
+  c(
+    "here",
+    "devtools",
+    "dplyr",
+    "DT",
+    "rtweet",
+    "workflowr"
+  )
+
+new.packages <-
+  list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+
+if (length(new.packages) > 0)
+  install.packages(new.packages)
