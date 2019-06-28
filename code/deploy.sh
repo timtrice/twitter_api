052f378ee40bee1eab1fdd401be9d0ec9df9b0dc
Tim Trice <tim.trice@gmail.com>
2019-06-28 08:12:28 -0500

    Build docs in develop branch

--- code/deploy.sh
+++ code/deploy.sh
@@ -4,10 +4,11 @@ git config --global user.name "Travis CI"
 git config --global user.email "tim.trice@gmail.com"
 git clone https://github.com/timtrice/twitter_api.git
 cd twitter_api
+git checkout develop
 Rscript --verbose code/02_retrieve.R
 rm -rf docs
 Rscript -e 'workflowr::wflow_build(update = TRUE, view = FALSE, verbose = TRUE)'
 git add .
 MSG="Rebuild docs, $(date)"
 git commit -m "$MSG"
-git push $FULL_REPO
+git push --force $FULL_REPO develop:master
